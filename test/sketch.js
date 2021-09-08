b2bdbf5af307d597095fa886880fa37e3c725a81
sumire0204 <makoti1203@gmail.com>
2021-09-08 23:56:39 +0900

    add test dir

---
+++ test/sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+    // createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
+	frameRate(8);
+}
+
+function draw(){
+	background(33);
+	fill(255);
+	circle(width/2, height/2, 100);
+}
